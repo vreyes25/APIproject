24d24e0abafd95380e13820c0149dc4880e0ec55
StephanZuniga <szaleman14@gmail.com>
2020-10-24 02:46:21 -0600

    Se agregó una pantalla y el código de transición entre pantallas

---
+++ app/src/main/java/com/example/nasaapi_project/FirstScreen.kt
@@ -0,0 +1,11 @@
+package com.example.nasaapi_project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FirstScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_first_screem)
+    }
+}
--- app/src/main/java/com/example/nasaapi_project/MainActivity.kt
+++ app/src/main/java/com/example/nasaapi_project/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.nasaapi_project
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun nextScreen(view: View) {
+        val change = Intent(this, FirstScreen::class.java)
+        startActivity(change)
     }
 }
