f59d8230b5cf7c356a494d9b2eaeca6a76c23772
Victor Reyes <victor.reyes1@ujcv.edu.hn>
2020-10-26 05:11:15 -0600

    Correción de Pantalla Principal

--- app/src/main/java/com/example/nasaapi_project/FirstScreen.kt
+++ app/src/main/java/com/example/nasaapi_project/FirstScreen.kt
@@ -1,11 +1,18 @@
 package com.example.nasaapi_project
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class FirstScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_first_screem)
+        setContentView(R.layout.activity_first_screen)
+    }
+
+    fun homeScreen(view: View){
+        val change = Intent(this, MainActivity::class.java)
+        startActivity(change)
     }
 }
