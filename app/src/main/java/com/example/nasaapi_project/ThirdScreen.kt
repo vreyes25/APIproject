bdfa152158983fe78acf32d13e29163da1a1eefd
Victor Reyes <victor.reyes1@ujcv.edu.hn>
2020-10-28 23:02:44 -0600

    Mejora del diseño de pantallas y colores de barra de estado

--- app/src/main/java/com/example/nasaapi_project/ThirdScreen.kt
+++ app/src/main/java/com/example/nasaapi_project/ThirdScreen.kt
@@ -1,11 +1,18 @@
 package com.example.nasaapi_project
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class ThirdScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_third_screen)
+    }
+
+    fun returnToGallery(view: View) {
+        val change = Intent(this, SecondScreen::class.java)
+        startActivity(change)
     }
 }
