c9278618b102e624f6a8d9a7dd7f0a5111db7ed4
StephanZuniga <szaleman14@gmail.com>
2020-10-31 22:45:18 -0600

    Diseño de primera pantalla y sus métodos

--- app/src/main/java/com/example/nasaapi_project/FirstScreen.kt
+++ app/src/main/java/com/example/nasaapi_project/FirstScreen.kt
@@ -4,11 +4,14 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import kotlinx.android.synthetic.main.activity_first_screen.*
 
 class FirstScreen : AppCompatActivity() {
+    private var daysCounter = 365
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_first_screen)
+        txt_days.text = getString(R.string.daysCounter, daysCounter)
     }
 
     fun homeScreen(view: View){
@@ -21,5 +24,19 @@ class FirstScreen : AppCompatActivity() {
         val change = Intent(this, SecondScreen::class.java)
         startActivity(change)
         overridePendingTransition(R.anim.fade_in, R.anim.fade_out)
+    }
+
+    fun lessCounter(view: View) {
+        if (daysCounter > 0){
+            daysCounter--
+        }
+        txt_days.text = getString(R.string.daysCounter, daysCounter)
+    }
+
+    fun plusCounter(view: View){
+        if (daysCounter < 365){
+            daysCounter++
+        }
+        txt_days.text = getString(R.string.daysCounter, daysCounter)
     }
 }
